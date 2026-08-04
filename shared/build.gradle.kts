11a4fb5bc377a2ff73ef0dcb6bd6944fdb2d26e1
CacheCi <155388387+cacheci@users.noreply.github.com>
2026-08-04 15:37:27 +0800

    gradle

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -2,13 +2,17 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
     alias(libs.plugins.kotlin.multiplatform)
-    alias(libs.plugins.android.library)
+    alias(libs.plugins.android.kotlin.multiplatform.library)
     alias(libs.plugins.jetbrains.compose)
     alias(libs.plugins.kotlin.compose)
 }
 
 kotlin {
-    androidTarget {
+    android {
+        namespace = "moe.https.syncthing.shared"
+        compileSdk = 37
+        minSdk = 26
+
         compilerOptions {
             jvmTarget.set(JvmTarget.JVM_17)
         }
@@ -25,19 +29,5 @@ kotlin {
             implementation(libs.miuix.ui)
             implementation(libs.miuix.icons)
         }
-    }
-}
-
-android {
-    namespace = "moe.https.syncthing.shared"
-    compileSdk = 37
-
-    defaultConfig {
-        minSdk = 26
-    }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
     }
 }
